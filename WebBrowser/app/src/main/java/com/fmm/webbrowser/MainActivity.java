305f3292df7a52def0ac30e6ccb9668624a2ec8f
kzeca <jcpeixotoleao@hotmail.com>
2020-08-08 23:38:13 -0400

    Add url search function

--- WebBrowser/app/src/main/java/com/fmm/webbrowser/MainActivity.java
+++ WebBrowser/app/src/main/java/com/fmm/webbrowser/MainActivity.java
@@ -2,13 +2,53 @@ package com.fmm.webbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    EditText link;
+    ImageButton search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setAll();
+        webView.setWebViewClient(new wvClient());
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = link.getText().toString();
+
+                webView.getSettings().setLoadsImagesAutomatically(true);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.loadUrl(url);
+            }
+        });
+    }
+
+    private class wvClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @SuppressLint("WrongViewCast")
+    void setAll() {
+        webView = findViewById(R.id.WebView);
+        link = findViewById(R.id.activity_main_et_link);
+        search = findViewById(R.id.activity_main_bt_search);
     }
 }
